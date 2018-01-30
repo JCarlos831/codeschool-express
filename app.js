d6c814b42205a5861e8dd88781831ec85104222d
Juan Montoya <jcmontoya@gmail.com>
2018-01-30 21:15:37 +0000

    Level 3 complete

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 var express = require('express');
 var app = express();
 
+var blocks = {
+    'Fixed': 'Fastened securely in position',
+    'Movable': 'Capable of being moved',
+    'Rotating': 'Moving in a circle around its center'
+};
+
 app.use(express.static('public'));
 
-app.get('/blocks', function(request, response){
-    var blocks = ['Fixed', 'Movable', 'Rotating'];
-    response.json(blocks);
+app.param('name', function(request, response, next) {
+    var name = request.params.name;
+    var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    
+    request.blockName = block;
+    
+    next();
+});
+
+app.get('/blocks', function(request, response) {
+    response.json(Object.keys(blocks));
+});
+
+app.get('/blocks/:name', function(request, response){
+    var description = blocks[request.blockName];
+    
+    if (!description) {
+        response.status(404).json('No description found for ' + request.params.name);
+    } else {
+        response.json(description);
+    }
+    
+    if (request.query.limit >= 0) {
+        response.json(blocks.slice(0, request.query.limit));
+    } else {
+        response.json(blocks);
+    }
 });
 
 app.listen(process.env.PORT);
